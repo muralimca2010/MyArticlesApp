0689ab7fc0df470e4bab1567a060802df3cab111
Muralidharan-Mohan-Vituity <muralidharan.m@robosoftin.com>
2021-10-18 12:42:44 +0530

    InitCommit

--- app/src/main/java/com/myarticlesapp/data/remote/ApiService.kt
+++ app/src/main/java/com/myarticlesapp/data/remote/ApiService.kt
@@ -2,9 +2,10 @@ package com.myarticlesapp.data.remote
 
 import com.myarticlesapp.domain.model.Articles
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 interface ApiService {
 
-    @GET("/posts")
+    @POST("/category_articles")
     suspend fun getPosts(): List<Articles>
 }
